5296c65a48af4708f97e77b2e06f5e32fc3887fc
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-01-20 17:55:11 +0530

    Create 217. Contains Duplicate.cpp

---
+++ Arrays/217. Contains Duplicate.cpp
@@ -0,0 +1,54 @@
+//O(n^2)
+class Solution1 {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+};
+
+//O(nlog(n))
+class Solution2 {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int index = 0;
+        while (index < nums.size()-1)
+        {
+            if (nums[index+1] == nums[index])
+                return true;
+        }
+        return false;
+    }
+};
+
+//O(nlog(n))
+class Solution3 {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        if (unique(nums.begin(), nums.end()) != nums.end())
+            return true;
+        return false;
+    }
+};
+
+//O(n)
+class Solution4 {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for (auto x: nums)
+            s.insert(x);
+        if (s.size() == nums.size())
+            return false;
+        return true;
+    }
+};
