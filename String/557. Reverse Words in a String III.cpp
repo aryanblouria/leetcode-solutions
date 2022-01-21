e96ba9c3adcdbc6ea7bfc7e07be3a94bb1a206ad
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-01-21 15:23:46 +0530

    Create 557. Reverse Words in a String III.cpp

---
+++ String/557. Reverse Words in a String III.cpp
@@ -0,0 +1,29 @@
+//O(n)
+class Solution {
+public:
+    string reverseString(string s) {
+        int i = 0, j = s.length()-1;
+        while (i < j)
+        {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+        return s;
+    }
+    string reverseWords(string s) {
+        vector<string> v;
+        string res, word;
+         
+        stringstream ss (s);
+        while (ss >> word)
+            v.push_back(reverseString(word));
+
+        for (int i = 0; i < v.size()-1; i++)
+            res += v[i] + " ";
+        res += v[v.size()-1];
+        return res;
+    }
+};
