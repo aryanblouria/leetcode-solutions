eff4689c5afef58305698f9cfadd8d2cc81367ad
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-01-25 22:34:57 +0530

    Update 0002. Add Two Numbers.cpp

--- Linked List/0002. Add Two Numbers.cpp
+++ Linked List/0002. Add Two Numbers.cpp
@@ -1,15 +1,10 @@
-//O(m+n)
-class Solution1 {
+//O(n)
+class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int carry = 0;
+        int carry = (l1->val + l2->val < 10) ? 0 : 1;
         ListNode* l3 = new ListNode;
-        l3->val = l1->val + l2->val;
-        if (l3->val >= 10) 
-        {
-            l3->val %= 10;
-            carry = 1;
-        }
+        l3->val = (l1->val + l2->val) % 10 ;
         l3->next = NULL;
         
         ListNode* p = l3;
@@ -20,36 +15,19 @@ public:
         {
             int sum = l1->val + l2->val + carry;
             ListNode* t = new ListNode;
-            if (sum < 10)
-            {
-                t->val = sum;
-                carry = 0; 
-            }
-            else
-            {
-                t->val = sum % 10;
-                carry = 1;
-            }  
+            t->val = sum % 10;
+            carry = (sum < 10) ? 0 : 1;
             p->next = t;
             p = t;
             l1 = l1->next;
             l2 = l2->next;
-        }
-        
+        } 
         while (l1 != NULL)
         {
             p->next = l1;
             int sum = l1->val + carry;
-            if (sum < 10)
-            {
-                 l1->val = sum;
-                 carry = 0;
-            }
-            else
-            {
-                l1->val = sum % 10;
-                carry = 1;
-            }
+            l1->val = sum % 10;
+            carry = (sum < 10) ? 0 : 1;
             l1 = l1->next;
             p = p->next;
         }
@@ -57,16 +35,8 @@ public:
         {
             p->next = l2;
             int sum = l2->val + carry;
-            if (sum < 10)
-            {
-                l2->val = sum;
-                carry = 0;   
-            }
-            else
-            {
-                l2->val = sum % 10;
-                carry = 1;
-            }
+            l2->val = sum % 10;
+            carry = (sum < 10) ? 0 : 1;
             l2 = l2->next;
             p = p->next;
         }
