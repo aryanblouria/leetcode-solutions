f280bc1040ba0df7680c045458eced38a7f6c80c
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-01-28 22:57:25 +0530

    Create 1281. Subtract Product and Sum of Digits.cpp

---
+++ Math/1281. Subtract Product and Sum of Digits.cpp
@@ -0,0 +1,15 @@
+//O(log10(n))
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int sum = 0, product = 1;
+        while (n > 0)
+        {
+            int digit = n % 10;
+            sum += digit;
+            product *= digit;
+            n /= 10;
+        }
+        return product - sum;
+    }
+};
