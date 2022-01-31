0613c3b699bd3557b607453d59754b81c3f28ceb
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-01-31 23:24:39 +0530

    Create 1486. XOR Operation in an Array.cpp

---
+++ Math/1486. XOR Operation in an Array.cpp
@@ -0,0 +1,14 @@
+//O(n)
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        vector<int> nums (n);
+        int x = 0;
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] = start + 2 * i;
+            x ^= nums[i];
+        }
+        return x;
+    }
+};
