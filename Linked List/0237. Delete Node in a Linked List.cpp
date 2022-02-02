00397f5a929a4f9b7003eccea43f9ab9df553366
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-02-02 16:38:45 +0530

    Create 0237. Delete Node in a Linked List.cpp

---
+++ Linked List/0237. Delete Node in a Linked List.cpp
@@ -0,0 +1,24 @@
+//O(n)
+class Solution1 {
+public:
+    void deleteNode(ListNode* node) {
+        while (node->next != NULL)
+        {
+            node->val = node->next->val;
+            if (node->next->next == NULL) 
+                node->next = NULL;
+            else
+                node = node->next;
+        }
+    }
+};
+
+//O(n)
+class Solution2 {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode *next = node->next;
+        *node = *next;
+        delete next;
+    }
+};
