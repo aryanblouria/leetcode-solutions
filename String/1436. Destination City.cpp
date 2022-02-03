db571bc30b383963425819d9751a78c6ab66f81b
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-02-03 15:42:18 +0530

    Create 1436. Destination City.cpp

---
+++ String/1436. Destination City.cpp
@@ -0,0 +1,17 @@
+//O(n)
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        map<string, int> m;
+        for (int i = 0; i < paths.size(); i++)
+        {
+            m[paths[i][0]]++;
+            m[paths[i][1]]--;
+        }
+        
+        for (auto x: m)
+            if (x.second == -1)
+                return x.first;
+        return "";
+    }
+};
