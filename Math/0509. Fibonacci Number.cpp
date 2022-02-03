06a58bd5debdfa310320d0dc311db60d4ede79de
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-02-04 05:26:03 +0530

    Create 0509. Fibonacci Number.cpp

--- Math/0509. Fibonacci Number.cpp
+++ Math/0509. Fibonacci Number.cpp
@@ -35,3 +35,15 @@ public:
         return second;
     }
 };
+
+//O(n)
+class Solution4 {
+public:
+    int fib(int n) {
+        static map<int, int> m;
+        if (m.find(n) != m.end()) return m[n];
+        if (n <= 1) return n;
+        m[n] = fib(n-1) + fib(n-2);
+        return m[n];
+    }
+};
