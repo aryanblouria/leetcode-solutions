141c527537a8e1323385eba3350bd475cb3dceaf
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-02-04 15:03:16 +0530

    Create 0169. Majority Element.cpp

---
+++ Hash Table/0169. Majority Element.cpp
@@ -0,0 +1,14 @@
+//O(n)
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int, int> m;
+        for (int i = 0; i < nums.size(); i++)
+            m[nums[i]]++;
+        
+        for (auto x: m)
+            if (x.second > (nums.size()/2))
+                return x.first;
+        return 0;
+    }
+};
