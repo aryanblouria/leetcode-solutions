b73f0d47105c2d11f4dd2c89f048b5c89ed3330a
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-05-29 14:31:38 +0530

    Create 0104. Maximum Depth of Binary Tree.cpp

---
+++ Tree/0104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,9 @@
+class Solution1 {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+        return max(lh, rh) + 1;
+    }
+};
