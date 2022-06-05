75720307af828e1186c897fbb7cda17a85256bf8
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-06-05 18:41:12 +0530

    Create 0050. Pow(x,n).cpp

--- Math/0050. Pow(x,n).cpp
+++ Math/0050. Pow(x,n).cpp
@@ -1,23 +1,20 @@
 //O(n)
 class Solution {
 public:
-    double myPow(double x, int n) {
-        double y = x;
-        double z = 1;
-        if (x == 0 || x == 1 || n == 1) return x;
-        if (n == 0) return 1;
-        if (n > 0) 
-        {
-            n--;
-            while (n--)
-                x *= y;
-        }
-        else 
-        {
-            while (n++)
-                z /= x;
-            x = z;
-        }
-        return x;
+    double pow(double x,int n)
+    {
+        if(n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+        if(n % 2 == 0)
+            return pow(x*x, n/2);
+        return x * pow(x*x, n/2);
     }
+
+    double myPow(double x, int n) {
+        if (n < 0)
+            return 1/pow(x, abs(n));
+        return pow(x, n);
+        }
 };
