49222613f7160ca8eaf8b85bc49c49bac1fb07d2
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-06-16 20:42:45 +0530

    Create 0003. Longest Substring of Non-Repeating Characters.cpp

---
+++ Hash Table/0003. Longest Substring of Non-Repeating Characters.cpp
@@ -0,0 +1,20 @@
+//O(n)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> st;
+        int i = 0, j = 0, n = s.length(), ans = 0;
+        
+        while (i < n && j < n)
+        {
+            if (st.find(s[j]) == st.end())
+            {
+                st.insert(s[j++]);
+                ans = max(ans, j - i);
+            }
+            else
+                st.erase(s[i++]);
+        }
+        return ans;
+    }
+};
