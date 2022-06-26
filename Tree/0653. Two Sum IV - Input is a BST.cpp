008314cce4d431ab2224572014f6b6fa427cf903
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2022-06-26 23:41:55 +0530

    Create 0653. Two Sum IV - Input is a BST.cpp

---
+++ Tree/0653. Two Sum IV - Input is a BST.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool dfs(TreeNode* root, unordered_set<int> &s, int k)
+    {
+         if (root == NULL) return false;
+         if (s.count(k - root->val)) return true;
+         s.insert(root->val);
+         return dfs(root->left, s, k) || dfs(root->right, s, k);
+    }
+    
+    bool findTarget(TreeNode* root, int k) 
+    {
+        unordered_set<int> s;
+        return dfs(root, s, k);
+    }
+};
